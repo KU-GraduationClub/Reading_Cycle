b0bb4a289752df7fedebc2a6fedad45580909fc6
KHSSE <113084857+KHSSE@users.noreply.github.com>
2023-12-26 19:15:51 +0900

    feat : post 브랜치 관련 백그라운드 컬러, 툴바용 스타일, 테마 추가

--- app/src/main/java/com/example/reading_cycle/MainActivity.kt
+++ app/src/main/java/com/example/reading_cycle/MainActivity.kt
@@ -2,10 +2,50 @@ package com.example.reading_cycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+
+    var newFragment: Fragment? = null
+    var oldFragment: Fragment? = null
+    companion object{
+        val POST_MAIN_FRAGMENT = "PostMainFragment"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+//        fun replaceFragment(name:String, addToBackStack:Boolean, bundle:Bundle? = null) {
+//
+//            SystemClock.sleep(200)
+//
+//            // Fragment 교체 상태로 설정한다.
+//            val fragmentTransaction = supportFragmentManager.beginTransaction()
+//
+//            // newFragment 에 Fragment가 들어있으면 oldFragment에 넣어준다.
+//            if(newFragment != null){
+//                oldFragment = newFragment
+//            }
+//
+//            // 새로운 Fragment를 담을 변수
+//            newFragment = when(name){
+//                POST_MAIN_FRAGMENT -> PostMainFragment()
+//                else -> Fragment()
+//            }
+//
+//            // Fragment를 교체한다.
+//            fragmentTransaction.replace(R.id.navHostFragmentMain, newFragment!!)
+//
+//            if (addToBackStack == true) {
+//                // Fragment를 Backstack에 넣어 이전으로 돌아가는 기능이 동작할 수 있도록 한다.
+//                fragmentTransaction.addToBackStack(name)
+//            }
+//
+//            // 교체 명령 동작.
+//            fragmentTransaction.commit()
+//        }
+     }
 }
+
