c903fa32bdb3ec5013b65b87d25bad98988948ae
KHSSE <113084857+KHSSE@users.noreply.github.com>
2023-12-29 18:55:06 +0900

    feat : Main 액티비티 임시 설정, 아이콘 추가

--- app/src/main/java/com/example/reading_cycle/MainActivity.kt
+++ app/src/main/java/com/example/reading_cycle/MainActivity.kt
@@ -17,35 +17,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        fun replaceFragment(name:String, addToBackStack:Boolean, bundle:Bundle? = null) {
-//
-//            SystemClock.sleep(200)
-//
-//            // Fragment 교체 상태로 설정한다.
-//            val fragmentTransaction = supportFragmentManager.beginTransaction()
-//
-//            // newFragment 에 Fragment가 들어있으면 oldFragment에 넣어준다.
-//            if(newFragment != null){
-//                oldFragment = newFragment
-//            }
-//
-//            // 새로운 Fragment를 담을 변수
-//            newFragment = when(name){
-//                POST_MAIN_FRAGMENT -> PostMainFragment()
-//                else -> Fragment()
-//            }
-//
-//            // Fragment를 교체한다.
-//            fragmentTransaction.replace(R.id.navHostFragmentMain, newFragment!!)
-//
-//            if (addToBackStack == true) {
-//                // Fragment를 Backstack에 넣어 이전으로 돌아가는 기능이 동작할 수 있도록 한다.
-//                fragmentTransaction.addToBackStack(name)
-//            }
-//
-//            // 교체 명령 동작.
-//            fragmentTransaction.commit()
-//        }
+        // 기본 ActionBar를 숨깁니다.
+        supportActionBar?.hide()
+
+        replaceFragment(POST_MAIN_FRAGMENT, false, null)
+    }
+
+        fun replaceFragment(name:String, addToBackStack:Boolean, bundle:Bundle? = null) {
+
+            SystemClock.sleep(200)
+
+            // Fragment 교체 상태로 설정한다.
+            val fragmentTransaction = supportFragmentManager.beginTransaction()
+
+            // newFragment 에 Fragment가 들어있으면 oldFragment에 넣어준다.
+            if(newFragment != null){
+                oldFragment = newFragment
+            }
+
+            // 새로운 Fragment를 담을 변수
+            newFragment = when(name){
+                POST_MAIN_FRAGMENT -> PostMainFragment()
+                else -> Fragment()
+            }
+
+            // Fragment를 교체한다.
+            fragmentTransaction.replace(R.id.hostFragmentMain, newFragment!!)
+
+            if (addToBackStack == true) {
+                // Fragment를 Backstack에 넣어 이전으로 돌아가는 기능이 동작할 수 있도록 한다.
+                fragmentTransaction.addToBackStack(name)
+            }
+
+            // 교체 명령 동작.
+            fragmentTransaction.commit()
+        }
      }
-}
+
+
 
