3c0712e622c2297804059475cafc4f55c3b91c9b
KHSSE <113084857+KHSSE@users.noreply.github.com>
2023-12-29 18:55:29 +0900

    feat : Post 관련 툴바, 상단 앱바 관련 설정

--- app/src/main/java/com/example/reading_cycle/PostMainFragment.kt
+++ app/src/main/java/com/example/reading_cycle/PostMainFragment.kt
@@ -5,14 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import com.example.reading_cycle.databinding.FragmentPostMainBinding
 
 class PostMainFragment : Fragment() {
+
+    private lateinit var fragmentPostMainBinding : FragmentPostMainBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_post_main, container, false)
+        fragmentPostMainBinding = FragmentPostMainBinding.inflate(inflater)
+        // 타이틀 아이콘 작업
+        val iconDrawable = ContextCompat.getDrawable(requireContext(), R.drawable.baseline_recycling_title_40)
+        fragmentPostMainBinding.toolbarTitle.setCompoundDrawablesWithIntrinsicBounds(iconDrawable, null, null, null)
+        fragmentPostMainBinding.toolbarTitle.compoundDrawablePadding = resources.getDimensionPixelSize(R.dimen.icon_text_padding)
+        // 텍스트 설정
+        fragmentPostMainBinding.toolbarTitle.text = "Reading\nCycle"
+
+
+        return fragmentPostMainBinding.root
     }
 }
