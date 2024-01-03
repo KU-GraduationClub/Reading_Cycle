fea0cd87522ac1f54083e1d8b027b0da89041202
KHSSE <113084857+KHSSE@users.noreply.github.com>
2024-01-03 18:12:05 +0900

    feat : Sale, Swap 화면 툴바 제작 및 관련 아이콘 추가, 설정 등

--- app/src/main/java/com/example/reading_cycle/SalePostFragment.kt
+++ app/src/main/java/com/example/reading_cycle/SalePostFragment.kt
@@ -5,19 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.example.reading_cycle.databinding.FragmentAddSalePostBinding
 import com.example.reading_cycle.databinding.FragmentAddSwapPostBinding
 import com.example.reading_cycle.databinding.FragmentSalePostBinding
 
 class SalePostFragment : Fragment() {
 
+    lateinit var mainActivity: MainActivity
     private lateinit var fragmentSalePostBinding: FragmentSalePostBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        mainActivity = activity as MainActivity
         fragmentSalePostBinding = FragmentSalePostBinding.inflate(inflater)
+
+        // 뒤로 가기 버튼 클릭 리스너 설정
+        fragmentSalePostBinding.toolbarLayoutSalePost.setNavigationOnClickListener {
+            // 뒤로 가기 버튼을 눌렀을 때의 동작
+            // mainActivity.removeFragment(MainActivity.ADD_SALE_POST_FRAGMENT)
+        }
+
+        // 타이틀 아이콘 및 텍스트 설정
+        val iconDrawable = ContextCompat.getDrawable(requireContext(), R.drawable.baseline_sync_40_blue)
+        fragmentSalePostBinding.toolbarTitleSalePost.setCompoundDrawablesWithIntrinsicBounds(iconDrawable, null, null, null)
+        fragmentSalePostBinding.toolbarTitleSalePost.compoundDrawablePadding = resources.getDimensionPixelSize(R.dimen.icon_text_padding)
+        fragmentSalePostBinding.toolbarTitleSalePost.text = "도서 교환"
 
         return fragmentSalePostBinding.root
     }
--- app/src/main/java/com/example/reading_cycle/SwapPostFragment.kt
+++ app/src/main/java/com/example/reading_cycle/SwapPostFragment.kt
@@ -5,18 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.example.reading_cycle.databinding.FragmentSalePostBinding
 import com.example.reading_cycle.databinding.FragmentSwapPostBinding
 
 class SwapPostFragment : Fragment() {
 
+    lateinit var mainActivity: MainActivity
     private lateinit var fragmentSwapPostBinding: FragmentSwapPostBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        mainActivity = activity as MainActivity
         fragmentSwapPostBinding = FragmentSwapPostBinding.inflate(inflater)
+
+        // 뒤로 가기 버튼 클릭 리스너 설정
+        fragmentSwapPostBinding.toolbarLayoutSwapPost.setNavigationOnClickListener {
+            // 뒤로 가기 버튼을 눌렀을 때의 동작
+            // mainActivity.removeFragment(MainActivity.ADD_SALE_POST_FRAGMENT)
+        }
+
+        // 타이틀 아이콘 및 텍스트 설정
+        val iconDrawable = ContextCompat.getDrawable(requireContext(), R.drawable.baseline_currency_exchange_40_red)
+        fragmentSwapPostBinding.toolbarTitleSwapPost.setCompoundDrawablesWithIntrinsicBounds(iconDrawable, null, null, null)
+        fragmentSwapPostBinding.toolbarTitleSwapPost.compoundDrawablePadding = resources.getDimensionPixelSize(R.dimen.icon_text_padding)
+        fragmentSwapPostBinding.toolbarTitleSwapPost.text = "도서 판매"
 
         return fragmentSwapPostBinding.root
     }
