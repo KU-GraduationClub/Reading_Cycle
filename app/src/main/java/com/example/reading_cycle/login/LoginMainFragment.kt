436482fca4dd7676d2fdf2d25bc83142c4b22824
Yoonbeom <znf47529@gmail.com>
2024-01-16 16:29:09 +0900

    feat : 화면전환(뒤로가기)

--- app/src/main/java/com/example/reading_cycle/login/LoginMainFragment.kt
+++ app/src/main/java/com/example/reading_cycle/login/LoginMainFragment.kt
@@ -1,31 +1,42 @@
 package com.example.reading_cycle.login
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import com.example.reading_cycle.R
+import com.example.reading_cycle.MainActivity
 import com.example.reading_cycle.databinding.FragmentLoginMainBinding
-
-
 class LoginMainFragment : Fragment() {
 
+    lateinit var mainActivity: MainActivity
     private lateinit var fragmentLoginMainBinding : FragmentLoginMainBinding
+    private fun setupUiListeners() {
+        fragmentLoginMainBinding.registerBtn.setOnClickListener {// 회원가입 버튼
+
+            mainActivity.replaceFragment(MainActivity.Companion.USER_REGISTER_FRAGMENT, true, null)
+        }
+
+        fragmentLoginMainBinding.searchPWBtn.setOnClickListener {// 비밀번호 찾기 버튼
+
+            mainActivity.replaceFragment(MainActivity.Companion.PASSWORD_RESET_FRAGMENT, true, null)
+        }
+
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        mainActivity = activity as MainActivity
         fragmentLoginMainBinding = FragmentLoginMainBinding.inflate(inflater)
-        /* 타이틀 아이콘 작업
-        val iconDrawable =
-            ContextCompat.getDrawable(requireContext(), R.drawable.baseline_arrow_back_ios_28)
-        fragmentLoginMainBinding.toolbarTitle.setCompoundDrawablesWithIntrinsicBounds(iconDrawable, null, null, null)
-        // 텍스트 설정
-        fragmentLoginMainBinding.toolbarTitle.text = ""*/
 
+        setupUiListeners()
 
         return fragmentLoginMainBinding.root
+        }
+
     }
-}
+
+
--- app/src/main/java/com/example/reading_cycle/login/UserRegisterFragment.kt
+++ app/src/main/java/com/example/reading_cycle/login/UserRegisterFragment.kt
@@ -4,28 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import com.example.reading_cycle.R
+import com.example.reading_cycle.MainActivity
 import com.example.reading_cycle.databinding.FragmentUserRegisterBinding
 
 class UserRegisterFragment : Fragment() {
 
-    private lateinit var fragmentUserRegisterBinding : FragmentUserRegisterBinding
+    lateinit var mainActivity: MainActivity
+    private lateinit var fragmentUserRegisterBinding: FragmentUserRegisterBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        mainActivity = activity as MainActivity
         fragmentUserRegisterBinding = FragmentUserRegisterBinding.inflate(inflater)
-        // 타이틀 아이콘 작업
-        val iconDrawable =
-            ContextCompat.getDrawable(requireContext(), R.drawable.baseline_arrow_back_ios_28)
-        fragmentUserRegisterBinding.toolbarTitle.setCompoundDrawablesWithIntrinsicBounds(iconDrawable, null, null, null)
+
         // 텍스트 설정
         fragmentUserRegisterBinding.toolbarTitle.text = "회원가입"
 
+        fragmentUserRegisterBinding.run {
+            // 뒤로가기 버튼 클릭 시 프래그먼트 제거
+            toolbarUserRegister.setNavigationOnClickListener {
+                mainActivity.removeFragment(MainActivity.USER_REGISTER_FRAGMENT)
+            }
 
-        return fragmentUserRegisterBinding.root
+            return fragmentUserRegisterBinding.root
+        }
     }
 }
