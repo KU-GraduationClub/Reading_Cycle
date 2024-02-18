1ade834ff2b5a10dcba3c58405d84034f4e5a3c4
KHSSE <113084857+KHSSE@users.noreply.github.com>
2024-02-18 21:30:24 +0900

    1차 Merge 작업 완료 (2024/02/18)

---
+++ app/src/main/java/com/example/reading_cycle/library/model/FriendDataClass.kt
@@ -0,0 +1,45 @@
+package com.example.reading_cycle.library.model
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.reading_cycle.R
+
+data class FriendDataClass(val title: String)
+
+class FriendMainAdapter(private val friendList: List<FriendDataClass>) : RecyclerView.Adapter<FriendMainAdapter.FriendViewHolder>() {
+    // View Type 상수 정의
+    private val VIEW_TYPE_FRIEND = 0
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FriendViewHolder {
+        val view = when (viewType) {
+            VIEW_TYPE_FRIEND -> {
+                LayoutInflater.from(parent.context)
+                    .inflate(R.layout.row_friend_item_layout, parent, false)
+            }
+            else -> throw IllegalArgumentException("Invalid view type")
+        }
+        return FriendViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return friendList.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return VIEW_TYPE_FRIEND
+    }
+
+    override fun onBindViewHolder(holder: FriendViewHolder, position: Int) {
+        holder.bind(friendList[position])
+    }
+
+    // FriendViewHolder 구현
+    class FriendViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(friendData: FriendDataClass) {
+            // 아이템 데이터를 뷰에 바인딩하는 로직을 작성합니다.
+        }
+    }
+}
