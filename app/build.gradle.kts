07bf7ce7a783ad595911402c8e33c46c19f8dc76
Owner <Owner@OWNER-PC>
2024-02-18 18:17:57 +0900

    feat : LocSetFragment XML 화면 임시 작성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,14 @@ plugins {
 }
 
 android {
+    signingConfigs {
+        getByName("debug") {
+            keyPassword = "android"
+            keyAlias = "androiddebugkey"
+            storePassword = "android"
+            storeFile = file("D:\\Android\\.android\\debug.keystore")
+        }
+    }
     namespace = "com.example.reading_cycle"
     compileSdk = 34
 
@@ -33,14 +41,20 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    viewBinding{
+        enable = true
+    }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.android.gms:play-services-location:21.1.0")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/reading_cycle/MainActivity.kt
+++ app/src/main/java/com/example/reading_cycle/MainActivity.kt
@@ -1,11 +1,99 @@
 package com.example.reading_cycle
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.reading_cycle.databinding.ActivityMainBinding
+import com.example.reading_cycle.location.model.LocSetFragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+
+
+
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private var mMap: GoogleMap? = null
+    var newFragment: Fragment? = null
+    var oldFragment: Fragment? = null
+    private lateinit var mainBinding: ActivityMainBinding
+
+
+    companion object {
+        val LOC_SET_FRAGMENT = "LocSetFragment"
+    }
+
+    fun replaceFragment(name: String, addToBackStack: Boolean, bundle: Bundle? = null) {
+
+        SystemClock.sleep(200)
+
+        // Fragment 교체 상태로 설정한다.
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+
+        // newFragment 에 Fragment가 들어있으면 oldFragment에 넣어준다.
+        if (newFragment != null) {
+            oldFragment = newFragment
+        }
+
+        // 새로운 Fragment를 담을 변수
+        newFragment = when (name) {
+            LOC_SET_FRAGMENT -> LocSetFragment()
+
+            else -> Fragment()
+        }
+
+        // Fragment를 교체한다.
+        fragmentTransaction.replace(R.id.hostFragmentMain, newFragment!!)
+
+        if (addToBackStack == true) {
+            // Fragment를 Backstack에 넣어 이전으로 돌아가는 기능이 동작할 수 있도록 한다.
+            fragmentTransaction.addToBackStack(name)
+        }
+
+        // 교체 명령 동작.
+        fragmentTransaction.commit()
+    }
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+
+
+        // Hide the default ActionBar
+        supportActionBar?.hide()
+
+        // Replace the fragment
+        replaceFragment(LOC_SET_FRAGMENT, false)
+    }
+
+
+    // Handle the Google Map when it's ready
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        val SEOUL = LatLng(37.556, 126.97)
+        val markerOptions = MarkerOptions()
+        markerOptions.position(SEOUL)
+        markerOptions.title("서울")
+        markerOptions.snippet("한국 수도")
+        mMap?.addMarker(markerOptions)
+        mMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 10f))
     }
 }
+
+
+
+
+
+
+
+
+
+
