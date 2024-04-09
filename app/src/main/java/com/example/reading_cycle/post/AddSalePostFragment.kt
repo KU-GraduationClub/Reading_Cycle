2256feb727e603b4258929a88dc0d2365b82f6ee
KHSSE <113084857+KHSSE@users.noreply.github.com>
2024-04-09 18:50:51 +0900

    feat : addSalePost - CardView 활용 이미지 추가 기능 구현(카메라/갤러리)

--- app/src/main/java/com/example/reading_cycle/post/AddSalePostFragment.kt
+++ app/src/main/java/com/example/reading_cycle/post/AddSalePostFragment.kt
@@ -1,13 +1,23 @@
 package com.example.reading_cycle.post
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupMenu
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.example.reading_cycle.MainActivity
 import com.example.reading_cycle.R
 import com.example.reading_cycle.databinding.FragmentAddSalePostBinding
@@ -16,6 +26,11 @@ class AddSalePostFragment : Fragment() {
 
     private lateinit var mainActivity: MainActivity
     private lateinit var fragmentAddSalePostBinding: FragmentAddSalePostBinding
+
+    companion object {
+        const val REQUEST_IMAGE_CAPTURE = 1001
+        const val REQUEST_PICK_IMAGE = 1002
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,12 +45,99 @@ class AddSalePostFragment : Fragment() {
             mainActivity.removeFragment(MainActivity.ADD_SALE_POST_FRAGMENT)
         }
 
-        // FrameLayout 클릭 이벤트 처리
-        fragmentAddSalePostBinding.FrameAddSalePost1.setOnClickListener {
-            showBookTypeMenu(it)
+        // 카드뷰 클릭 이벤트 처리
+        fragmentAddSalePostBinding.cardViewAddSalePostImg.setOnClickListener {
+            showImageSourceDialog()
         }
 
         return fragmentAddSalePostBinding.root
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            REQUEST_PICK_IMAGE -> {
+                if (resultCode == RESULT_OK) {
+                    val selectedImageUri = data?.data
+                    selectedImageUri?.let { uri ->
+                        val imageBitmap = uriToBitmap(uri)
+                        imageBitmap?.let { bitmap ->
+                            val resizedBitmap = resizeBitmap(bitmap)
+                            fragmentAddSalePostBinding.imgAddSalePost1.setImageBitmap(resizedBitmap)
+                        }
+                    }
+                }
+            }
+
+            REQUEST_IMAGE_CAPTURE -> {
+                if (resultCode == RESULT_OK) {
+                    // 이미지 캡처의 경우 URI가 아닌 비트맵을 반환하므로 바로 사용 가능
+                    val imageBitmap = data?.extras?.get("data") as Bitmap
+                    val resizedBitmap = resizeBitmap(imageBitmap)
+                    fragmentAddSalePostBinding.imgAddSalePost1.setImageBitmap(resizedBitmap)
+                }
+            }
+        }
+    }
+    private fun resizeBitmap(bitmap: Bitmap): Bitmap {
+        val targetWidth = fragmentAddSalePostBinding.imgAddSalePost1.width
+        val targetHeight = fragmentAddSalePostBinding.imgAddSalePost1.height
+
+        val width = bitmap.width
+        val height = bitmap.height
+
+        val scaleX = targetWidth.toFloat() / width
+        val scaleY = targetHeight.toFloat() / height
+        val scaleFactor = Math.max(scaleX, scaleY)
+
+        val scaledWidth = scaleFactor * width
+        val scaledHeight = scaleFactor * height
+
+        return Bitmap.createScaledBitmap(bitmap, scaledWidth.toInt(), scaledHeight.toInt(), true)
+    }
+
+    private fun uriToBitmap(uri: Uri): Bitmap? {
+        return try {
+            val inputStream = requireContext().contentResolver.openInputStream(uri)
+            BitmapFactory.decodeStream(inputStream)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+
+    private fun showImageSourceDialog() {
+        val items = arrayOf("갤러리", "카메라")
+        val builder = android.app.AlertDialog.Builder(requireContext())
+        builder.setItems(items) { dialog, which ->
+            when (which) {
+                0 -> openGallery()
+                1 -> openCamera()
+            }
+        }
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, REQUEST_PICK_IMAGE)
+    }
+
+    private fun openCamera() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+            ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(android.Manifest.permission.CAMERA),
+                REQUEST_IMAGE_CAPTURE
+            )
+        } else {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
+        }
     }
 
     private fun showBookTypeMenu(view: View) {
@@ -48,65 +150,45 @@ class AddSalePostFragment : Fragment() {
             // 각 메뉴 아이템에 대한 처리 추가
             when (menuItem.itemId) {
                 R.id.menuNovel -> {
-                    updateButtonText("소설")
-                }
+                    updateButtonText("소설") }
                 R.id.menuPoetry -> {
-                    updateButtonText("시")
-                }
+                    updateButtonText("시") }
                 R.id.menuEssay -> {
-                    updateButtonText("에세이")
-                }
+                    updateButtonText("에세이") }
                 R.id.menuClassic -> {
-                    updateButtonText("고전")
-                }
+                    updateButtonText("고전") }
                 R.id.menuComic -> {
-                    updateButtonText("만화")
-                }
+                    updateButtonText("만화") }
                 R.id.menuChildren -> {
-                    updateButtonText("어린이")
-                }
+                    updateButtonText("어린이") }
                 R.id.menuToddler -> {
-                    updateButtonText("유아")
-                }
+                    updateButtonText("유아") }
                 R.id.menuSelfDevelopment -> {
-                    updateButtonText("자기계발")
-                }
+                    updateButtonText("자기계발") }
                 R.id.menuReference -> {
-                    updateButtonText("학습/참고서")
-                }
+                    updateButtonText("학습/참고서") }
                 R.id.menuMajor -> {
-                    updateButtonText("전공서")
-                }
+                    updateButtonText("전공서") }
                 R.id.menuCooking -> {
-                    updateButtonText("요리/제빵")
-                }
+                    updateButtonText("요리/제빵") }
                 R.id.menuLanguage -> {
-                    updateButtonText("외국어")
-                }
+                    updateButtonText("외국어") }
                 R.id.menuSocialScience -> {
-                    updateButtonText("사회/과학")
-                }
+                    updateButtonText("사회/과학") }
                 R.id.menuArt -> {
-                    updateButtonText("예술")
-                }
+                    updateButtonText("예술") }
                 R.id.menuReligion -> {
-                    updateButtonText("종교")
-                }
+                    updateButtonText("종교") }
                 R.id.menuEconomics -> {
-                    updateButtonText("경제/경영")
-                }
+                    updateButtonText("경제/경영") }
                 R.id.menuHealthTravel -> {
-                    updateButtonText("건강/여행")
-                }
+                    updateButtonText("건강/여행") }
                 R.id.menuHistory -> {
-                    updateButtonText("역사")
-                }
+                    updateButtonText("역사") }
                 R.id.menuPhilosophy -> {
-                    updateButtonText("철학")
-                }
+                    updateButtonText("철학") }
                 R.id.menuOther -> {
-                    updateButtonText("기타")
-                }
+                    updateButtonText("기타") }
             }
             true
         }
