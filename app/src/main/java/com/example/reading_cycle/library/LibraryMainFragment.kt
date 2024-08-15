609fdf66ff409879a107056ee644af1a7f9651c5
Yoonbeom <znf47529@gmail.com>
2024-08-16 00:41:16 +0900

    feat: 라이브러리 UI 수정 Refactor: LibraryMainFragment 정리 MainActivity에서 해당 Binding 제거 Remove: LibraryMyFragment.kt fragment_library_my.xml

--- app/src/main/java/com/example/reading_cycle/library/LibraryMainFragment.kt
+++ app/src/main/java/com/example/reading_cycle/library/LibraryMainFragment.kt
@@ -1,62 +1,46 @@
 package com.example.reading_cycle.library
 
-import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.reading_cycle.MainActivity
 import com.example.reading_cycle.R
-import com.example.reading_cycle.databinding.FragmentLibraryMyBinding
+import com.example.reading_cycle.databinding.FragmentLibraryMainBinding
+
 class LibraryMainFragment : Fragment() {
 
     private lateinit var mainActivity: MainActivity
-    private lateinit var fragmentLibraryMyBinding: FragmentLibraryMyBinding
-
-    companion object {
-        fun newInstance(bundle: Bundle?): LibraryMainFragment {
-            val fragment = LibraryMainFragment()
-            fragment.arguments = bundle
-            return fragment
-        }
-    }
+    private lateinit var fragmentLibraryMainBinding: FragmentLibraryMainBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         mainActivity = activity as MainActivity
-        fragmentLibraryMyBinding = FragmentLibraryMyBinding.inflate(inflater, container, false)
+        fragmentLibraryMainBinding = FragmentLibraryMainBinding.inflate(inflater, container, false)
         mainActivity.showBottomNavigation()
 
         // 타이틀 아이콘 작업
         ContextCompat.getDrawable(requireContext(), R.drawable.baseline_sync_40)
-        fragmentLibraryMyBinding.toolbarLibraryMyTitle.setCompoundDrawablesWithIntrinsicBounds(
+        fragmentLibraryMainBinding.toolbarLibraryMainTitle.setCompoundDrawablesWithIntrinsicBounds(
             null,
             null,
             null,
             null
         )
-        fragmentLibraryMyBinding.toolbarLibraryMyTitle.compoundDrawablePadding =
+        fragmentLibraryMainBinding.toolbarLibraryMainTitle.compoundDrawablePadding =
             resources.getDimensionPixelSize(
                 R.dimen.icon_text_padding
             )
         // 텍스트 설정
-        fragmentLibraryMyBinding.toolbarLibraryMyTitle.text = "라이브러리"
-
-
-
-        // RecyclerView 설정
-        val layoutManager = LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
-        fragmentLibraryMyBinding.recyclerViewLibraryMy.layoutManager = layoutManager
-
+        fragmentLibraryMainBinding.toolbarLibraryMainTitle.text = "라이브러리"
 
         // 툴바 알림 메뉴 클릭 이벤트 처리
-        fragmentLibraryMyBinding.toolbarLayoutLibraryMy.setOnMenuItemClickListener { menuItem ->
+        fragmentLibraryMainBinding.toolbarLayoutLibraryMain.setOnMenuItemClickListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.libraryMenuItemNotify -> {
                     mainActivity.navigateToNotifyFragment()
@@ -66,31 +50,25 @@ class LibraryMainFragment : Fragment() {
             }
         }
 
-        return fragmentLibraryMyBinding.root
+        // RecyclerView 설정
+        setupRecyclerView()
+
+        return fragmentLibraryMainBinding.root
     }
 
-    private fun showPostTypeDialog() {
-        val builder = AlertDialog.Builder(requireContext())
-        builder.setTitle("게시글 유형을 선택해 주세요")
+    private fun setupRecyclerView() {
+        // RecyclerView에 사용될 GridLayoutManager 설정
+        val layoutManager = GridLayoutManager(requireContext(), 3)
+        fragmentLibraryMainBinding.recyclerViewLibraryMain.layoutManager = layoutManager
 
-        val postTypes = arrayOf("교환 게시글", "판매 게시글")
+        // 어댑터에 사용할 이미지 리스트 (예시)
+        val imageList = listOf(
+            R.drawable.border_add_post_blue,
+            // 필요한 만큼 이미지 추가
+        )
 
-        builder.setItems(postTypes) { _, which ->
-            // 사용자가 선택한 항목에 따라 해당 프래그먼트로 이동하는 로직 추가
-            when (which) {
-                0 -> (requireActivity() as MainActivity).replaceFragment(
-                    MainActivity.ADD_SWAP_POST_FRAGMENT,
-                    true
-                )
-                1 -> (requireActivity() as MainActivity).replaceFragment(
-                    MainActivity.ADD_SALE_POST_FRAGMENT,
-                    true
-                )
-            }
-        }
-
-        val dialog = builder.create()
-        dialog.show()
+        // LibraryAdapter를 사용하여 RecyclerView에 어댑터 설정
+        val adapter = LibraryAdapter(requireContext(), imageList)
+        fragmentLibraryMainBinding.recyclerViewLibraryMain.adapter = adapter
     }
 }
-
