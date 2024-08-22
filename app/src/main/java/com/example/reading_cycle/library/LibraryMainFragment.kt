d496a32a6adb68314d06a11eab4e734ac0d7f74f
Yoonbeom <znf47529@gmail.com>
2024-08-22 20:30:23 +0900

    a commit to pull

--- app/src/main/java/com/example/reading_cycle/library/LibraryMainFragment.kt
+++ app/src/main/java/com/example/reading_cycle/library/LibraryMainFragment.kt
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.GridLayoutManager
 import com.example.reading_cycle.MainActivity
 import com.example.reading_cycle.R
 import com.example.reading_cycle.databinding.FragmentLibraryMainBinding
+import com.example.reading_cycle.library.model.LibraryMainAdapter
 
 class LibraryMainFragment : Fragment() {
 
@@ -68,7 +69,7 @@ class LibraryMainFragment : Fragment() {
         )
 
         // LibraryAdapter를 사용하여 RecyclerView에 어댑터 설정
-        val adapter = LibraryAdapter(requireContext(), imageList)
+        val adapter = LibraryMainAdapter(requireContext(), imageList)
         fragmentLibraryMainBinding.recyclerViewLibraryMain.adapter = adapter
     }
 }
